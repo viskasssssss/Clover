8525e09052af2a6bd88813da05c3d4ebd3787ae5
viskasssssss <ratmirt59@gmail.com>
2026-01-30 12:30:50 +0500

    Fixed resizing

--- Clover/src/Platform/OpenGL/OpenGLRendererAPI.h
+++ Clover/src/Platform/OpenGL/OpenGLRendererAPI.h
@@ -8,6 +8,7 @@ namespace Clover
 	{
 		virtual void Init() override;
 
+		virtual void SetViewport(uint32_t x, uint32_t y, uint32_t width, uint32_t height) override;
 		virtual void SetClearColor(const Color& color) override;
 		virtual void Clear() override;
 
